aba1f22dcf55164f141a6050f2c524f5e3985963
Gregory Halverson <gregory.h.halverson@jpl.nasa.gov>
2023-05-23 18:14:09 -0700

    fixing VIIRS docker

--- drought/run_viirs_product.sh
+++ drought/run_viirs_product.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 echo "starting VIIRS product generation container"
+cd /drought
+git pull
+make reinstall-soft
+cd /new_mexico_VIIRS
 New-Mexico-VIIRS-Server --working /new_mexico_VIIRS --static /static --GEOS5FP /GEOS5FP --LANCE /LANCE --SRTM /SRTM --output /VIIRS_output
